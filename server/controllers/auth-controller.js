28b47b9db62d76ed42b1552d0cdd0d7fa0796757
Eman Tahir <emantahir053@gmail.com>
2023-12-02 00:34:41 +0500

    Added Json web token

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,41 +1,39 @@
 const User = require("../models/user-model");
-const bcrypt = require("bcryptjs");
 
 const home = async (req, res) => {
-    try {
-        res.status(200).json("Hi from Controllers")
-    } catch (error) {
-        res.status(400).json(`An error Occurs ${error}`)
-        console.log(error);
-        
-    }
+  try {
+    res.status(200).json("Hi from Controllers");
+  } catch (error) {
+    res.status(400).json(`An error Occurs ${error}`);
+    console.log(error);
+  }
 };
-// register 
-const register = async (req, res)=> {
-    try {
-        const {username, email, phone, password} = req.body;
+// register
+const register = async (req, res) => {
+  try {
+    const { username, email, phone, password } = req.body;
 
-        const userExist = await User.findOne({email});
+    const userExist = await User.findOne({ email });
 
-        if (userExist) {
-            return res.status(400).json({msg: "Email already Exist"})
-        }
-
-
-        // hash password 
-        const saltRound = 10;
-        const hash_password = await bcrypt.hash(password, saltRound) 
-
-        const userCreated = await User.create({ username, email, phone, password: hash_password })
-
-        res.status(200).json({userCreated })
-    } catch (error) {
-        res.status(500).json(`An error Occurs ${error}`)
-        console.log(error);
-        
+    if (userExist) {
+      return res.status(400).json({ msg: "Email already Exist" });
     }
-}
 
+    // hash password
 
+    const userCreated = await User.create({ username, email, phone, password });
 
-module.exports = { home, register};
+    res
+      .status(201)
+      .json({
+        msg: "Registration Successfull",
+        token: await userCreated.generateToken(),
+        userId: userCreated._id.toString(),
+      });
+  } catch (error) {
+    res.status(500).json(`An error Occurs ${error}`);
+    console.log(error);
+  }
+};
+
+module.exports = { home, register };
