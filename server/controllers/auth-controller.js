3feba3036ce1bfa09912e7578b7e7628b48e4f79
Eman Tahir <emantahir053@gmail.com>
2023-12-16 22:44:48 +0500

    Login token Authorization

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/user-model");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
 const home = async (req, res) => {
   try {
@@ -24,13 +24,11 @@ const register = async (req, res) => {
 
     const userCreated = await User.create({ username, email, phone, password });
 
-    res
-      .status(201)
-      .json({
-        msg: "Registration Successfull",
-        token: await userCreated.generateToken(),
-        userId: userCreated._id.toString(),
-      });
+    res.status(201).json({
+      msg: "Registration Successfull",
+      token: await userCreated.generateToken(),
+      userId: userCreated._id.toString(),
+    });
   } catch (error) {
     res.status(500).json(`An error Occurs ${error}`);
     console.log(error);
@@ -38,32 +36,47 @@ const register = async (req, res) => {
 };
 
 // login
-const login = async(req, res) => {
-try {
-  const {email, password} = req.body;
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  const userExist = await User.findOne({email});
+    const userExist = await User.findOne({ email });
 
-  if (!userExist) {
-    return res.status(401).json({msg: "Invalid Credentials"});    
-  }
-
-  // const user = await bcrypt.compare(password, userExist.password);
-  const isPasswordMatch = await userExist.comparePassword(password);
-
-
-  if (isPasswordMatch) {
-    res.status(200).json({msg: "Log in Successfull",
-    token: await userExist.generateToken(),
-    userId: userExist._id.toString(),
-  })
-    }else{
-      res.status(401).json({msg: "Invalid Credentials"})
+    if (!userExist) {
+      return res.status(401).json({ msg: "Invalid Credentials" });
     }
 
-} catch (error) {
-  res.status(500).json(`An error occurs ${error}`)
-}
+    // const user = await bcrypt.compare(password, userExist.password);
+    const isPasswordMatch = await userExist.comparePassword(password);
+
+    if (isPasswordMatch) {
+      res
+        .status(200)
+        .json({
+          msg: "Log in Successfull",
+          token: await userExist.generateToken(),
+          userId: userExist._id.toString(),
+        });
+    } else {
+      res.status(401).json({ msg: "Invalid Credentials" });
+    }
+  } catch (error) {
+    res.status(500).json(`An error occurs ${error}`);
+  }
+};
+
+// get user data
+const user = async(req,res) => {
+
+  try {
+    const userData  = req.user;
+    console.log(userData);
+    return res.status(200).json({ msg: userData})
+  } catch (error) {
+    console.log(`error from the get route ${error}`);
+  }
+
 }
 
-module.exports = { home, register, login};
+
+module.exports = { home, register, login, user };
--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-// controllers 
-const {home, register, login} = require("../controllers/auth-controller")
+// controllers
+const { home, register, login, user } = require("../controllers/auth-controller");
 // validate Schema
-const SignupSchema = require ("../validators/auth-validator")
-// middleware 
+const SignupSchema = require("../validators/auth-validator");
+// middleware
 const validate = require("../middlewares/validate-middleware");
-
+const authMiddleware = require("../middlewares/auth-middleware");
 
 router.route("/").get(home);
-router.route("/register").post( validate(SignupSchema), register)
-router.route("/login").post(login)
+router.route("/register").post(validate(SignupSchema), register);
+router.route("/login").post(login);
+router.route("/user").get(authMiddleware ,user);
 
 module.exports = router;
-
